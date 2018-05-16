121d82cc3939a71cc816cdc9b55e46e709e76029
TikiXiang <tikixiang@163.com>
2018-05-16 17:52:06 +0800

    init

---
+++ src/main/java/com/tiki/bootway/SpringbootwayApplication.java
@@ -0,0 +1,12 @@
+package com.tiki.bootway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tiki/bootway/SpringbootwayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tiki.bootway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootwayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
